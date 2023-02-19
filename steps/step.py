285c271937dddb83915c9fbeea3c5df2d76dd0ee
hansss <ohs2251@naver.com>
2023-02-19 17:01:45 +0900

    step9: Make func comfortable

--- steps/step.py
+++ steps/step.py
@@ -3,6 +3,10 @@ import numpy as np
 
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+            
         self.data = data
         self.grad = None
         self.creator = None
@@ -11,6 +15,9 @@ class Variable:
         self.creator = func
 
     def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
         funcs = [self.creator]
         while funcs:                    # Improve efficiency
             f = funcs.pop()             # Get a function
@@ -21,11 +28,17 @@ class Variable:
                 funcs.append(x.creator)
 
 
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
-        output = Variable(y)
+        output = Variable(as_array(y))
         output.set_creator(self)   
         self.input = input
         self.output = output       
@@ -58,15 +71,20 @@ class Exp(Function):
         gx = np.exp(x) * gy
         return gx
 
-A = Square()
-B = Exp()
-C = Square()
+
+def square(x):
+    return Square()(x)
+
+
+def exp(x):
+    return Exp()(x)
+
 
 x = Variable(np.array(0.5))
-a = A(x)
-b = B(a)
-y = C(b)
-
-y.grad = np.array(1.0)
+y = square(exp(square(x)))
 y.backward()
 print(x.grad)
+
+x = Variable(np.array(1.0)) # OK
+x = Variable(None)          # OK
+x = Variable(1.0)           # NG
