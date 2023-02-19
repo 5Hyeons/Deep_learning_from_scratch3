92249545dae1e4c0e8fe9319e2aad8446b133e91
hansss <ohs2251@naver.com>
2023-02-19 16:35:55 +0900

    step4: 수치 미분

--- steps/step.py
+++ steps/step.py
@@ -9,6 +9,8 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        self.input = input
+        self.output = output
         return output
     
     def forward(self, x):
@@ -21,14 +23,25 @@ class Square(Function):
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
-    
-A = Square()
-B = Exp()
-C = Square()
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+f = Square()
+x = Variable(np.array(2.0))
+dy = numerical_diff(f, x)
+print(dy)
+
+def f(x):
+    A = Square()
+    B = Exp()
+    C = Square()
+    return C(B(A(x)))
 
 x = Variable(np.array(0.5))
-a = A(x)
-b = B(a)
-y = C(b)
-
-print(y.data)
+dy = numerical_diff(f, x)
+print(dy)
