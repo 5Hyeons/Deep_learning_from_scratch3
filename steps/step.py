ea40177e3d8f5b6352cc629989cca21fa191cd06
hansss <ohs2251@naver.com>
2023-02-19 16:43:53 +0900

    step7: backward automation

--- steps/step.py
+++ steps/step.py
@@ -1,16 +1,31 @@
 import numpy as np
 
+
 class Variable:
     def __init__(self, data):
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        f = self.creator
+        if f is not None:
+            x = f.input
+            x.grad = f.backward(self.grad)
+            x.backward()
+
 
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
         self.input = input
+        self.output = output
         return output
     
     def forward(self, x):
@@ -19,9 +34,11 @@ class Function:
     def backward(self, gy):
         raise NotImplementedError()
 
+
 class Square(Function):
     def forward(self, x):
-        return x ** 2
+        y = x ** 2
+        return y
 
     def backward(self, gy):
         x = self.input.data
@@ -30,7 +47,8 @@ class Square(Function):
 
 class Exp(Function):
     def forward(self, x):
-        return np.exp(x)
+        y = np.exp(x)
+        return y
     
     def backward(self, gy):
         x = self.input.data
@@ -47,7 +65,5 @@ b = B(a)
 y = C(b)
 
 y.grad = np.array(1.0)
-b.grad = C.backward(y.grad)
-a.grad = B.backward(b.grad)
-x.grad = A.backward(a.grad)
+y.backward()
 print(x.grad)
