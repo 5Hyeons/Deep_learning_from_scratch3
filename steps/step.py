ab91dd2cb7e8d040917ebb0315de82e4a6670b99
hansss <ohs2251@naver.com>
2023-02-20 23:09:17 +0900

    step22: Operator overload3

--- steps/step.py
+++ steps/step.py
@@ -170,17 +170,102 @@ def mul(x0, x1):
     return Mul()(x0, x1)
 
 
+class Neg(Function):
+    def forward(self, x):
+        return -x
+    
+    def backward(self, gy):
+        return -gy
+    
+
+def neg(x):
+    return Neg()(x)
+
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0 - x1
+        return y
+    
+    def backward(self, gy):
+        return gy, -gy
+    
+
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return sub(x1, x0)
+
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0 / x1
+        return y
+    
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+    
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return div(x1, x0)
+
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+
+    def forward(self, x):
+        y = x ** self.c
+        return y
+    
+    def backward(self, gy):
+        x = self.inputs[0].data
+        c = self.c
+
+        gx = c * x ** (c - 1) * gy
+        return gy
+    
+def pow(x, c):
+    return Pow(c)(x)
+
+
 Variable.__add__ = add
 Variable.__radd__ = add
 Variable.__mul__ = mul
 Variable.__rmul__ = mul
 
+Variable.__neg__ = neg
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+Variable.__pow__ = pow
+
 x = Variable(np.array(2.0))
-y = x + np.array(3.0)
+y = -x
 print(y)
 
-y = x + 3.0
+y1 = 2.0 - x
+y2 = x - 1.0
+print(y1)
+print(y2)
+
+y = 3.0 / x
 print(y)
 
-y = 3.0 * x + 1.0
+y = x ** 3
+y.backward()
 print(y)
