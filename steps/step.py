c4856bae6c8c7b6c360843f9868c1c5218a1ef4c
hansss <ohs2251@naver.com>
2023-03-01 21:16:12 +0900

    step28: Functions optimization

--- steps/step.py
+++ steps/step.py
@@ -1,16 +1,34 @@
 if '__file__' in globals():
     import os, sys
-    print(__file__)
-    print(os.path.dirname(__file__))
-    print(os.path.join(os.path.dirname(__file__), '..'))
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import numpy as np
 from dezero import Variable
 
-x = Variable(np.array(1.0))
-y = (x + 3) ** 2
-y.backward()
+import dezero
+if not dezero.is_simple_core:
+    from dezero.core_simple import Variable
+    from dezero.core_simple import setup_variable
+    setup_variable()
 
-print(y)
-print(x.grad)
+def rosenbrock(x0, x1):
+    y = 100 * (x1 - x0 ** 2) ** 2 + (x0 - 1) ** 2
+    return y
+
+
+x0 = Variable(np.array(0.0))
+x1 = Variable(np.array(2.0))
+lr = 0.001
+iters = 1000
+
+for i in range(iters):
+    print(x0, x1)
+
+    y = rosenbrock(x0, x1)
+
+    x0.cleargrad()
+    x1.cleargrad()
+    y.backward()
+
+    x0.data -= lr * x0.grad
+    x1.data -= lr * x1.grad
