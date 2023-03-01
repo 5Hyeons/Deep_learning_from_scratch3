3d7ebfa7f26fa46c51b2b842698dfc66625929c3
hansss <ohs2251@naver.com>
2023-03-01 21:27:36 +0900

    step33: Optimization using newton's law (active)

--- steps/step.py
+++ steps/step.py
@@ -15,9 +15,6 @@ def f(x):
     y = x ** 4 - 2 * x ** 2
     return y
 
-def gx2(x):
-    return 12 * x ** 2 - 4
-
 
 x = Variable(np.array(2.0))
 iters = 10
@@ -27,6 +24,11 @@ for i in range(iters):
 
     y = f(x)
     x.cleargrad()
-    y.backward()
+    y.backward(create_graph=True)
+
+    gx = x.grad
+    x.cleargrad()
+    gx.backward()
+    gx2 = x.grad
 
     x.data -= x.grad / gx2(x.data)
